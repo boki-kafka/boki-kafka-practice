4db37cf409be91ddce4625214357f34230563509
seung.bok <seungbok.lee@huinnoaim.com>
2024-04-01 18:30:48 +0900

    Initial commit

---
+++ src/main/java/SimpleProducer.java
@@ -0,0 +1,34 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        // 1 - 설정 주입
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        // 2 - Producer 레코드 생성
+        // => offset이 없다. 나중에 이 Record가 브로커(리더 파티션)에 저장될 때 offset이 지정된다
+        String messageValue = "testMessage";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, messageValue);
+        producer.send(record); // 여기서 Accumulator로 들어간다
+        logger.info("{}", record);
+        producer.flush(); // Accumulator에 있는 배치를 바로 전송한다
+        producer.close();
+    }
+}
