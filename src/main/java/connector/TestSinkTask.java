c2f82c46d9f3a54aa47354a11dee2ee688d66465
boki <lsb530@naver.com>
2024-04-06 03:09:34 +0900

    connector

---
+++ src/main/java/connector/TestSinkTask.java
@@ -0,0 +1,36 @@
+package connector;
+
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTask;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class TestSinkTask extends SinkTask {
+    @Override
+    public String version() {
+        return null;
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+
+    }
+
+    @Override
+    public void put(Collection<SinkRecord> collection) {
+
+    }
+
+    @Override
+    public void flush(Map<TopicPartition, OffsetAndMetadata> currentOffsets) {
+        super.flush(currentOffsets);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
